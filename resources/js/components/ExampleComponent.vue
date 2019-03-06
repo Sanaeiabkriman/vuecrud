b77d546770d374c8f932f224f1fbafe67cb9ee1c
Sanaei <iabkriman.sanae@gmail.com>
2019-03-06 16:01:42 +0100

    vuecrud

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Example Component</div>
+
+                    <div class="card-body">
+                        YAAAAAAAAAAAAAy
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Ceci est un console.log.')
+        }
+    }
+</script>
